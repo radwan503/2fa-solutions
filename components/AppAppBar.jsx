a5b7da2b682a684b85ef132e7bbf4062c0a9ec36
radwan503 <radwancse503@gmail.com>
2024-06-05 23:29:45 +0600

    landing page

--- components/AppAppBar.jsx
+++ components/AppAppBar.jsx
@@ -51,19 +51,19 @@ export default function AppAppBar({ mode, toggleColorMode }) {
       <Container maxWidth="lg">
         <Toolbar
           variant="regular"
-        // sx={(theme) => ({
-        //   display: 'flex',
-        //   alignItems: 'center',
-        //   justifyContent: 'space-between',
-        //   flexShrink: 0,
-        //   borderRadius: '999px',
-        //   bgcolor: 'hsla(220, 60%, 99%, 0.6)',
-        //   backdropFilter: 'blur(24px)',
-        //   maxHeight: 40,
-        //   border: '1px solid',
-        //   borderColor: 'divider',
-        //   boxShadow: '0 1px 2px hsla(210, 0%, 0%, 0.05), 0 2px 12px hsla(210, 100%, 80%, 0.5)',
-        // })}
+          sx={(theme) => ({
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexShrink: 0,
+            borderRadius: '999px',
+            bgcolor: 'hsla(220, 60%, 99%, 0.6)',
+            backdropFilter: 'blur(24px)',
+            maxHeight: 40,
+            border: '1px solid',
+            borderColor: 'divider',
+            boxShadow: '0 1px 2px hsla(210, 0%, 0%, 0.05), 0 2px 12px hsla(210, 100%, 80%, 0.5)',
+          })}
         >
           <Box
             sx={{
